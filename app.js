592695c6e57fd1e9bb7d42582d7045c0914b18b6
Sperthix <matusbuci@gmail.com>
2021-07-27 16:58:52 +0200

    hadik sa zvacsuje, kontrola koncu hry, restart cez enter

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const showScore = document.querySelector('.score');
+const finalMessage = document.querySelector('h1');
 
 const snakeSize = 30;
 let snakeSpeed = snakeSize;
 let snakePosX = 0;
 let snakePosY = canvas.height/2;
+
+let chvost = [];
 
 let velkostX = canvas.width / snakeSize;
 let velkostY = canvas.height / snakeSize;
@@ -17,33 +20,60 @@ let foodPosX = 0;
 let foodPosY = 0;
 
 let score = 0;
+let gameIsRunning = true;
 
 
-document.addEventListener('keydown', smer)
+document.addEventListener('keydown', smer);
 
 
 function gameloop() {
-    //kontrola okraju
+
+    if(gameIsRunning){
+        kontrola();
+        zapisHada();
+        render();
+        pohyb();
+        setTimeout(gameloop, 1000 / 15);
+        // requestAnimationFrame(gameloop);
+    }
+}
+
+function kontrola(){
+    kontrolaOkraju();
+    kontrolaScorovania();
+    kontrolaKoncaHry();
+}
+
+function kontrolaOkraju(){
     if(snakePosX >= canvas.width) snakePosX = 0; 
     if(snakePosX <= 0-snakeSize) snakePosX = canvas.width;
     if(snakePosY >= canvas.height) snakePosY = 0;
     if(snakePosY <= 0-snakeSize) snakePosY = canvas.height;
+}
 
-    //kontrola papania
+function kontrolaScorovania(){
     if ((snakePosX === foodPosX) && (snakePosY === foodPosY)){
         score++;
         randomSpot();
         showScore.textContent = score;
     }
-
-    render();
-    pohyb();
-    setTimeout(gameloop, 1000 / 15);
-    // requestAnimationFrame(gameloop);
 }
 
+function kontrolaKoncaHry(){
+    chvost.forEach((castChvosta) => {
+        if (score > 1){
+            if (snakePosX === castChvosta.x && snakePosY === castChvosta.y){
+                gameIsRunning = false;
+                finalMessage.textContent = `Dosiahol si skore: ${score}!`;
+            }
+        }
+        
+    })
+}
+
+
 function smer(event){
-    // alert(event.key);
+    // console.log(event.key);
     switch(event.key){
         case 'ArrowUp':
             if(smerY != 1){
@@ -69,16 +99,30 @@ function smer(event){
                 smerY = 0;
             }
             break;
+            /*Z tohto chcem urobit pauzu*/
         case 'Escape':
             smerX = 0;
             smerY = 0;
-            break;   
+            if(gameIsRunning)gameIsRunning = false;
+            else{
+                gameIsRunning = true;
+                gameloop();
+            }
+            break;
+            //reset hry
+        case 'Enter':
+            if (! gameIsRunning) location.reload();  
     }
 }
 
 function pohyb(){
     snakePosX+= snakeSpeed * smerX;
     snakePosY+= snakeSpeed * smerY;
+}
+
+function zapisHada() {
+    chvost.push({x: snakePosX, y: snakePosY});
+    chvost = chvost.slice(-1 * score);
 }
 
 
@@ -111,16 +155,35 @@ function mriezka(){
 
 //vykreslenie hadika
 function snake(){
+    if (score > 1){
+        chvost.forEach(part => {
+            rectangle('#4D4949' , part.x, part.y, snakeSize, snakeSize);
+        });
+    }
     rectangle('black', snakePosX, snakePosY, snakeSize, snakeSize);
 }
 
 
 //generacia pozicie pre jedlo
 function randomSpot(){
-    foodPosX = Math.floor(Math.random() * velkostX) * snakeSize;
-    foodPosY = Math.floor(Math.random() * velkostY) * snakeSize;
+    let x = Math.floor(Math.random() * velkostX) * snakeSize;
+    let y = Math.floor(Math.random() * velkostY) * snakeSize;
+    console.log(x, y);
+    if (chvost.length > 0)
+    {
+        chvost.forEach(cast => {
+            if (cast.x === x && cast.y === y) randomSpot();
+            else{
+                foodPosX = x;
+                foodPosY = y;
+            }
+        })
+    }
+    else{
+        foodPosX = x;
+        foodPosY = y;
+    }
 }
-
 
 randomSpot();
 render();
